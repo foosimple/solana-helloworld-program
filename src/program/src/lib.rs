f8a2aa1cda2e0031181b0e23bfe38c84ebdd1233
Arunoda Susiripal <arunoda.susiripala@gmail.com>
2022-01-07 14:58:42 +0530

    Add the program logic

--- src/program/src/lib.rs
+++ src/program/src/lib.rs
@@ -6,15 +6,36 @@ use solana_program::{
     program_error::ProgramError,
     msg
 };
+use borsh::{BorshDeserialize, BorshSerialize};
 
 entrypoint!(run_program);
+
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct CounterData {
+    pub value: i32,
+}
 
 pub fn run_program(
     program_id: &Pubkey,
     accounts: &[AccountInfo],
     _instructions: &[u8]
 ) -> ProgramResult {
-    msg!("Executing the program of id: {}", program_id);
+    if accounts.len() == 0 {
+        msg!("No accounts found!");
+        return Err(ProgramError::AccountDataTooSmall);
+    }
+
+    let account = &accounts[0];
+    if account.owner != program_id {
+        msg!("Data owner should be the program owner");
+        return Err(ProgramError::IllegalOwner);
+    }
+
+    let mut counter = CounterData::try_from_slice(&account.data.borrow())?;
+    counter.value += 1;
+    counter.serialize(&mut &mut account.data.borrow_mut()[..])?;
+
+    msg!("Set the counter value to: {}", counter.value);
     
     Ok(())
 }
--- src/program/tests/lib.rs
+++ src/program/tests/lib.rs
@@ -1,3 +1,4 @@
+use borsh::BorshDeserialize;
 use solana_program::{
     account_info::{AccountInfo},
     pubkey::Pubkey,
@@ -6,7 +7,7 @@ use solana_program::{
 };
 use solana_program_test::*;
 use std::mem;
-use my_program::{ run_program };
+use my_program::{ run_program, CounterData };
 
 #[tokio::test]
 async fn test_ok() {
@@ -31,4 +32,67 @@ async fn test_ok() {
     let instruction_data: Vec<u8> = vec![];
     let result = run_program(&program_id, &accounts, &instruction_data);
     assert_eq!(result, Ok(()));
+}
+
+#[tokio::test]
+async fn test_noaccounts() {
+    let program_id = Pubkey::default();
+    let accounts: Vec<AccountInfo> = Vec::new();
+    let instruction_data: Vec<u8> = Vec::new();
+
+    let result = run_program(&program_id, &accounts, &instruction_data);
+    assert_eq!(result, Err(ProgramError::AccountDataTooSmall));
+}
+
+#[tokio::test]
+async fn test_different_owner() {
+    let program_id = Pubkey::default();
+
+    let owner = Pubkey::new_unique();
+    let mut lamports = 0;
+    let mut data = vec![0; mem::size_of::<u32>()];
+    let account = AccountInfo::new(
+        &owner,
+        true,
+        true,
+        &mut lamports,
+        &mut data,
+        &owner,
+        false,
+        Epoch::default()
+    );
+
+    let instruction_data: Vec<u8> = Vec::new();
+    let accounts: Vec<AccountInfo> = vec![account];
+
+    let result = run_program(&program_id, &accounts, &instruction_data);
+    assert_eq!(result, Err(ProgramError::IllegalOwner));
+}
+
+#[tokio::test]
+async fn test_counter_value() {
+    let program_id = Pubkey::default();
+
+    let key = Pubkey::default();
+    let owner = Pubkey::default();
+    let mut lamports = 0;
+    let mut data = vec![0; mem::size_of::<u32>()];
+    let account = AccountInfo::new(
+        &key,
+        true,
+        true,
+        &mut lamports,
+        &mut data,
+        &owner,
+        false,
+        Epoch::default()
+    );
+
+    let accounts: Vec<AccountInfo> = vec![account];
+    let instruction_data: Vec<u8> = vec![];
+    run_program(&program_id, &accounts, &instruction_data).unwrap();
+    run_program(&program_id, &accounts, &instruction_data).unwrap();
+    
+    let counter = CounterData::try_from_slice(&data).unwrap();
+    assert_eq!(counter.value, 2);
 }
